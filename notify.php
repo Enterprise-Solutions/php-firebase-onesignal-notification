1f6534a2d9431991890dad215873ec85c26528a6
Alexa <albarrios98@outlook.com>
2019-08-14 16:45:25 -0400

    primer commit

---
+++ notify.php
@@ -0,0 +1,48 @@
+<?php
+class Notification{
+	private $title;
+	private $message;
+	private $image_url;
+	private $action;
+	private $action_destination;
+	private $data;
+	
+	function __construct(){
+         
+	}
+ 
+	public function setTitle($title){
+		$this->title = $title;
+	}
+ 
+	public function setMessage($message){
+		$this->message = $message;
+	}
+ 
+	public function setImage($imageUrl){
+		$this->image_url = $imageUrl;
+	}
+ 
+	public function setAction($action){
+		$this->action = $action;
+	}
+ 
+	public function setActionDestination($actionDestination){
+		$this->action_destination = $actionDestination;
+	}
+ 
+	public function setPayload($data){
+		$this->data = $data;
+	}
+	
+	public function getNotification(){
+		$notification = array();
+		$notification['title'] = $this->title;
+		$notification['message'] = $this->message;
+		$notification['image'] = $this->image_url;
+		$notification['action'] = $this->action;
+		$notification['action_destination'] = $this->action_destination;
+		return $notification;
+	}
+}
+?>
