a57c2b4cdb04b3843b464f4591ae8837c835c144
Alexa <albarrios98@outlook.com>
2019-08-28 15:33:53 -0400

    cambios

--- ObtenerCanales.php
+++ ObtenerCanales.php
@@ -1,22 +1,25 @@
-<?php 
-  header('Access-Control-Allow-Origin: *'); 
+<?php
+  header('Access-Control-Allow-Origin: *');
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-  
-  require_once("database.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
+
+  require_once("config/database.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
 
   $conexion = Database::db(); // CREA LA CONEXION
+
+    class Result {}
+    $response = new Result();
 
   // REALIZA LA QUERY A LA DB
   $query = "select * from canales order by id asc";
 			$sql = $conexion->prepare($query);
 			$sql->execute();
-      $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-    
+      $response->data = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-  $json = json_encode($resultado); // GENERA EL JSON CON LOS DATOS OBTENIDOS
 
-  
-  echo $json; // MUESTRA EL JSON GENERADO
-  
+  $data = json_encode($response); // GENERA EL JSON CON LOS DATOS OBTENIDOS
+
+
+  echo $data; // MUESTRA EL JSON GENERADO
+
   header('Content-Type: application/json');
 ?>
