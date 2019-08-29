2d5b352fe4f63772d22036f070cc0a8162327169
rudy <rudyca16@gmail.com>
2019-08-29 11:33:24 -0400

    Se creó ObtenerNotificaciones

---
+++ ObtenerNotificaciones.php
@@ -0,0 +1,26 @@
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  
+  require("./config/database.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
+
+  $conexion = Database::db(); // CREA LA CONEXION
+  $servicio = $_GET['id'];
+  class Result {}
+  $response = new Result();
+
+  // REALIZA LA QUERY A LA DB
+  $query = "select * from notificaciones where servicio = ? order by id asc";
+  $sql = $conexion->prepare($query);
+  $sql->execute();
+  $response->data = $sql->fetchAll(PDO::FETCH_ASSOC);
+    
+
+  $json = json_encode($response); // GENERA EL JSON CON LOS DATOS OBTENIDOS
+
+  
+  echo $json; // MUESTRA EL JSON GENERADO
+  
+  header('Content-Type: application/json');
+
+?>
